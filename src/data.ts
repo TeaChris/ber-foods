1b7e86965b40917f9f27e0c86e354d34e89fe200
bermuda <olasunkanmiboluwatife110@gmail.com>
2023-08-24 17:17:30 +0100

    order -- price -- products

--- src/data.ts
+++ src/data.ts
@@ -580,3 +580,34 @@ export const pizzas: Pizzas = [
     ],
   },
 ]
+
+type Single = {
+  id: number
+  title: string
+  desc: string
+  img: string
+  price: number
+  options?: { title: string; additionalPrice: number }[]
+}
+
+export const singleProduct: Single = {
+  id: 1,
+  title: 'Sicilian',
+  desc: 'Ignite your taste buds with a fiery combination of spicy pepperoni, jalapeños, crushed red pepper flakes, and melted mozzarella cheese, delivering a kick with every bite.',
+  img: '/images/p1.png',
+  price: 24.9,
+  options: [
+    {
+      title: 'Small',
+      additionalPrice: 0,
+    },
+    {
+      title: 'Medium',
+      additionalPrice: 4,
+    },
+    {
+      title: 'Large',
+      additionalPrice: 6,
+    },
+  ],
+}
